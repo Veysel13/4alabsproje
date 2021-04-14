ebe3957fdb4f3cc24ddc2b070974e3fc9aa8df41
unknown <veyselakpinar13@gmail.com>
2021-04-15 00:59:49 +0300

    repair

--- resources/views/layouts/alert.blade.php
+++ resources/views/layouts/alert.blade.php
@@ -1,9 +1,7 @@
 
 @php $message_type=['success','danger','warning','info'];@endphp
-
 @foreach($message_type as $item)
     @if(session()->has("$item"))
-
         <div class="row">
             <div class="col-md-12">
                 <div class="alert alert-{{$item}}">
@@ -11,5 +9,6 @@
                 </div>
             </div>
         </div>
+
     @endif
 @endforeach
